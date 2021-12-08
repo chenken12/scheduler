46d0e8ec428da120289b4a6666b66c9f92f3b9d3
Kenneth Chen <kenchen0606@gmail.com>
2021-12-07 20:03:19 -0500

    add scss to the li classname

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import "components/DayListItem.scss"
+import classNames from "classnames";
+
 export default function DayListItem(props) {
   const { name, spots, setDay } = props;
-  
+  let dayListClass = classNames('day-list__item', { 
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !spots
+ }); 
+
   return (
-    <li>
+    <li className={dayListClass}>
       <h2 className="text--regular">{name}</h2> 
       <h3 onClick={() => setDay(name)} className="text--light">{spots} spots remaining</h3>
     </li>
