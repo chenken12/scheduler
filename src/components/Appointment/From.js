ebee49c855cff8f797d775e58eaf5eb3cd3aaf07
Kenneth Chen <kenchen0606@gmail.com>
2021-12-08 22:56:40 -0500

    add states to from

--- src/components/Appointment/From.js
+++ src/components/Appointment/From.js
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "components/Appointment/styles.scss";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
-  const { student, interviewer, interviewers, onSave, onCancel } = props;
+  const { interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const cancel = function () {
+    reset();
+    onCancel();
+  }
+  const reset = function() {
+    setStudent("");
+    setInterviewer("");
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            
+
             value={ student }
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList 
           interviewers={interviewers}
           value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={onCancel} danger>Cancel</Button>
-          <Button onClick={onSave} confirm>Save</Button>
+          <Button onClick={() => cancel()} danger>Cancel</Button>
+          <Button onClick={() => onSave(student, interviewer)} confirm>Save</Button>
         </section>
       </section>
     </main>
