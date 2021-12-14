6bbd3fa9d8c563f535bcc24bff291ab9577b77e9
Kenneth Chen <kenchen0606@gmail.com>
2021-12-13 23:48:30 -0500

    the spots updates in sidebar now

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -28,6 +28,24 @@ export default function useApplicationData(initial) {
   // a function to change the days in the side
   const setDay = (day) => setState(prev => ({ ...prev, day }));
 
+  const countAppointments = function(id, appointments) {
+    const daysArr = [];
+    for (const [index, day] of state.days.entries()) {
+      if (day.appointments.includes(id)) {
+        let spots = 0;
+        for (const i of day.appointments) {
+          if (!appointments[i].interview) spots++;
+        }
+        
+        const newSpot = { ...state.days[index],spots: spots };
+        daysArr.push(newSpot);
+      } else {
+        daysArr.push(day);
+      }
+    }
+    return daysArr;
+  };
+
   function bookInterview(id, interview) {
     const appointment = {
       ...state.appointments[id],
@@ -39,8 +57,10 @@ export default function useApplicationData(initial) {
       [id]: appointment
     };
 
+    const spots = countAppointments(id, appointments);
+
     return axios.put(`/api/appointments/${id}`, { interview })
-      .then(() => setState({ ...state, appointments }))
+      .then(() => setState({ ...state, appointments, days:[...spots] }));
   };
 
   const cancelInterview = async function(id) {
@@ -49,8 +69,10 @@ export default function useApplicationData(initial) {
       [id]: { ...state.appointments[id], interview: null }
     };
 
+    const spots = countAppointments(id, appointments);
+
     return await axios.delete(`/api/appointments/${id}`)
-      .then(() => setState({ ...state, appointments }))
+      .then(() => setState({ ...state, appointments, days:[...spots] }))
   };
 
   return { state, setDay, bookInterview, cancelInterview };
